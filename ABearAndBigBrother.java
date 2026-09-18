e6fff36155c3c6e1f588a44e4d80e897837dff36
Bhadri Prabhu K <bhadrikeshav0612@gmail.com>
2026-09-18 22:42:54 +0530

    feat: solve Bear and Big Brother

---
+++ ABearAndBigBrother.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ABearAndBigBrother{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int y = 0;
+        while(a <= b){
+            a *= 3;
+            b *= 2;
+            y++;
+        }
+        System.out.println(y);
+    }
+}
